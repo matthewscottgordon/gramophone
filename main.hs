65a3d7316ea926851f2e2fab9a1f9a4f5da948e5
Matthew Gordon <gordon@qps.nl>
2012-12-19 22:38:51 -0400

    Now with label changing!

--- main.hs
+++ main.hs
@@ -6,7 +6,14 @@ main = do
      
      builder <- builderNew
      builderAddFromFile builder "MainWindow.glade"
+     
      mainWindow <- builderGetObject builder castToWindow "mainWindow"
      onDestroy mainWindow mainQuit
+     
+     statusLabel <- builderGetObject builder castToLabel "statusLabel"
+     displayMessageButton <- builderGetObject builder castToButton "displayMessageButton"
+     onClicked displayMessageButton $ do
+         set statusLabel [ labelText := "Button Clicked" ]
+
      widgetShowAll mainWindow
      mainGUI
