f8c1c8459763fbbdffe765fe4e4378a6f5ec19a5
Matthew Gordon <gordon@qps.nl>
2012-12-27 13:50:39 -0400

    Added "Create Database...", "Open Database...", and "Import Files..." buttons. Implemented "Create Database..." button. Need to clean up said implementation though.

--- main.hs
+++ main.hs
@@ -1,6 +1,8 @@
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Builder
 
+import Gramophone.Database as DB
+
 main = do
      initGUI
      
@@ -11,9 +13,26 @@ main = do
      onDestroy mainWindow mainQuit
      
      statusLabel <- builderGetObject builder castToLabel "statusLabel"
-     displayMessageButton <- builderGetObject builder castToButton "displayMessageButton"
-     onClicked displayMessageButton $ do
-         set statusLabel [ labelText := "Button Clicked" ]
+
+     createDatabaseButton <- builderGetObject builder castToButton "createDatabaseButton"
+     onClicked createDatabaseButton $ do
+         dl <- fileChooserDialogNew (Just "Save Database As...")
+                                    (Just mainWindow)
+                                    FileChooserActionSave
+                                    [("Save", ResponseOk), ("Cancel", ResponseCancel)]
+         r <- dialogRun dl
+         widgetHideAll dl
+         case r of
+           ResponseOk -> do maybeFilePath <- fileChooserGetFilename dl
+                            case maybeFilePath of
+                              Just filePath -> do DB.createNewDatabase filePath
+                                                  return ()
+                              Nothing       -> return ()
+           otherwise  -> return ()
+         widgetDestroy dl
+
+     --onClicked displayMessageButton $ do
+     --    set statusLabel [ labelText := "Button Clicked" ]
 
      widgetShowAll mainWindow
      mainGUI
