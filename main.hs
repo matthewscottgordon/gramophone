ce05d010b347159af7b36d3a4a9676209219294a
Matthew Gordon <gordon@qps.nl>
2013-01-28 21:30:40 -0400

    Clicking the "Import Files" button now displays a dialog to allow the user to select one or more files and the prints out the tags read from those files.

--- main.hs
+++ main.hs
@@ -1,8 +1,10 @@
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Builder
 
-import Gramophone.Database as DB
-import Gramophone.MediaController as MC
+import qualified Gramophone.Database as DB
+import qualified Gramophone.MediaController as MC
+
+import Control.Monad (forM_)
 
 data GUI = GUI {
       mainWindow :: Window,
@@ -23,7 +25,7 @@ initializeWidgets = do
   guiCreateDatabaseButton <- builderGetObject builder castToButton "createDatabaseButton"
 
   guiOpenDatabaseButton <- builderGetObject builder castToButton "openDatabaseButton"
-  guiImportFilesButton <- builderGetObject builder castToButton "createDatabaseButton"
+  guiImportFilesButton <- builderGetObject builder castToButton "importFilesButton"
 
   return (GUI guiMainWindow guiCreateDatabaseButton guiOpenDatabaseButton guiImportFilesButton)
 
@@ -45,11 +47,34 @@ onCreateDatabaseButtonClicked gui = do
 
 
 onImportFilesButtonClicked :: GUI -> DB.Connection -> IO ()
-onImportFilesButtonClicked gui db = return ()
+onImportFilesButtonClicked gui db = do
+  dl <- fileChooserDialogNew (Just "Files to import...")
+                             (Just (mainWindow gui))
+                             FileChooserActionOpen
+                             [("Import", ResponseOk), ("Cancel", ResponseCancel)]
+  fileChooserSetSelectMultiple dl True
+  r <- dialogRun dl
+  widgetHideAll dl
+  case r of
+    ResponseOk -> do filePaths <- fileChooserGetFilenames dl
+                     printTags filePaths
+    otherwise  -> return ()
+  widgetDestroy dl
+
+printTags :: [String] -> IO ()
+printTags filenames =
+    forM_ filenames $ \filename -> do
+        maybeTags <- MC.readTagsFromFile filename
+        case maybeTags of
+          Just tags -> do
+            putStrLn ( filename ++ ":" )
+            MC.printTags tags
+            putStrLn ""
+          Nothing -> putStrLn ( "No tags for \"" ++ filename ++ "\"" )
 
 setDatabase :: GUI -> DB.Connection -> IO ()
 setDatabase gui db = do
-  onClicked (importFilesButton gui) (onImportFilesButtonClicked gui db)
+  onClicked (importFilesButton gui) (onImportFilesButtonClicked gui db) 
   return ()
 
 main = do
