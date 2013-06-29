ea4af017818c5ac8a0e8d94156feb694f5be6705
Matthew Gordon <matthew.scott.gordon@gmail.com>
2013-06-28 22:50:20 -0300

    Work-in-progress writing database module
    Added numerous data types and functions. still in progress.

--- Gramophone/Database.hs
+++ Gramophone/Database.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE  OverloadedStrings #-}
+
 module Gramophone.Database 
     (
      Connection(),
@@ -5,8 +7,20 @@ module Gramophone.Database
      closeDatabase,
      openOrCreateDatabase,
      DatabaseRef(),
-     getDatabaseRef
+     getDatabaseRef,
+
+     Artist,
+     ArtistID(),
+     findArtist,
+     Album,
+     AlbumID(),
+     findAlbum,
+     Recording,
+     RecordingID()
     ) where
+
+import qualified Data.Text as T
+import Data.Text(Text)
 
 import Database.HDBC.Sqlite3 (connectSqlite3)
 import qualified Database.HDBC.Sqlite3 (Connection)
@@ -15,7 +29,38 @@ import Database.HDBC
 import Control.Monad
 import Data.Functor
 
+import Data.Convertible
+
 import System.Directory (doesFileExist)
+
+
+
+data RecordingID = RecordingID Integer deriving Show
+
+data Recording = Recording {
+     recordingId          :: RecordingID,
+     recordingFile        :: Text,
+     recordingTitle       :: Text,
+     recordingArtist      :: Artist,
+     recordingAlbum       :: Album,
+     recordingTrackNumber :: Integer
+} deriving Show
+
+data AlbumID = AlbumID Integer deriving Show
+
+data Album = Album {
+     albumId        :: AlbumID,
+     albumTitle     :: Text,
+     albumArtist    :: Artist,
+     albumNumTracks :: Integer
+} deriving Show
+
+data ArtistID = ArtistID Integer deriving Show
+
+data Artist = Artist {
+     artistId   :: ArtistID,
+     artistName :: Text
+} deriving Show
 
 
 data Connection = Connection Database.HDBC.Sqlite3.Connection
@@ -83,16 +128,79 @@ createNewDatabase filename = do
        []
   run conn
       "CREATE TABLE artists (\n\
-      \         id                  INTEGER PRIMARY KEY,\n\
+      \         id                  INTEGER,\n\
       \         name                VARCHAR(1024)\n\
       \    );\n"
       []
+  run conn
+      "CREATE TABLE last_ids (\n\
+      \         recording_id  INTEGER,\n\
+      \         album_id      INTEGER,\n\
+      \         artist_id     INTEGER\n\
+      \    );\n"
+      []
+  run conn "INSERT INTO last_ids (recording_id, album_id, artist_id) VALUES (0, 0, 0);" []
   commit conn
   return (Connection conn)
 
 withDatabase :: DatabaseRef -> ( Connection -> IO b ) -> IO b
 withDatabase (DatabaseRef filename) action = do
-  db <- openDatabase filename
-  r <- action db
-  closeDatabase db
+  Connection conn <- openDatabase filename
+  r <- action (Connection conn)
+  disconnect conn
   return r
+
+
+findArtist :: Text -> DatabaseRef -> IO [Artist]
+findArtist name db = withDatabase db $ findArtist' name
+
+findArtist' :: Text -> Connection -> IO [Artist]
+findArtist' name (Connection conn) = do
+    r <- quickQuery' conn "SELECT id, name FROM artists WHERE name = ?;" [convert name]
+    return $ map artistFromSql r
+  where artistFromSql (idValue:nameValue:[]) = Artist (ArtistID (convert idValue)) (convert nameValue)
+
+
+getArtist :: ArtistID -> DatabaseRef -> IO Artist
+getArtist a db = withDatabase db $ getArtist' a
+
+getArtist' :: ArtistID -> Connection -> IO Artist
+getArtist' (ArtistID i) (Connection conn) = do
+    r <- quickQuery' conn "SELECT name FROM artists WHERE id = ?;" [convert i]
+    case r of
+      [[name]] -> return $ Artist (ArtistID i) (convert name)
+
+data NewArtist = NewArtist {
+     newArtistName :: Text
+}
+
+addArtist :: NewArtist -> DatabaseRef -> IO (Maybe Artist)
+addArtist a db = withDatabase db $ addArtist' a
+
+addArtist' :: NewArtist -> Connection -> IO (Maybe Artist)
+addArtist' newArtist (Connection conn) = do
+    --newID <- fromIntegral . length <$> findArtist' (newArtistName newArtist) (Connection conn)
+    newID <- getNewArtistID' $ Connection conn
+    run conn "INSERT INTO artists (id, name) VALUES (?, ?);" [(convert newID), (convert $ newArtistName newArtist)]
+    commit conn
+    Just <$> getArtist' (ArtistID newID) (Connection conn)
+
+getNewArtistID' :: Connection -> IO Integer
+getNewArtistID' (Connection conn) = do
+    r <- quickQuery' conn "SELECT artist_id FROM last_ids" []
+    let [[oldID]] = r
+    let newID = (convert oldID) + 1;
+    run conn "UPDATE last_ids SET artist_id=?" [convert newID]
+    return newID
+
+findAlbum :: Text -> DatabaseRef -> IO [Album]
+findAlbum name db = do
+    let DatabaseRef filename = db
+    Connection conn <- openDatabase filename
+    r <- quickQuery conn "SELECT id, title, artist, num_tracks FROM artists WHERE name = ?;" [convert name]
+    r' <- mapM (albumFromSql db) r
+    disconnect conn
+    return r'
+  where albumFromSql db (idV:titleV:artistV:numTracksV:[]) = do
+                                            artist <- getArtist (ArtistID (convert artistV)) db
+                                            return $ Album (AlbumID (convert idV)) (convert titleV) artist (convert numTracksV)
