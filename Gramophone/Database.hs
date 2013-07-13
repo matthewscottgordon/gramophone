6fe637a2b1b270015bd04b6f537e6cb93775c875
Matthew Gordon <matthew.scott.gordon@gmail.com>
2013-07-13 13:13:56 -0300

    Add Haddock documentation for Database module.

--- Gramophone/Database.hs
+++ Gramophone/Database.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE  OverloadedStrings, MultiParamTypeClasses #-}
 
+-- |Create and manage the main Gramophone database.
 module Gramophone.Database 
     (
      DatabaseRef(),
@@ -35,9 +36,10 @@ import Data.Convertible
 import System.Directory (doesFileExist)
 
 
-
+-- |Opaque type containing a unique identifier for a Recording
 data RecordingID = RecordingID Integer deriving Show
 
+-- |Record describing an audio file
 data Recording = Recording {
      recordingId          :: RecordingID,
      recordingFile        :: Text,
@@ -47,8 +49,10 @@ data Recording = Recording {
      recordingTrackNumber :: Integer
 } deriving Show
 
+-- |Opaque type containing a unique identifier for an Album
 data AlbumID = AlbumID Integer deriving Show
 
+-- |Record describing an album
 data Album = Album {
      albumId        :: AlbumID,
      albumTitle     :: Text,
@@ -56,8 +60,10 @@ data Album = Album {
      albumNumTracks :: Integer
 } deriving Show
 
+-- |Opaque type containing a unique identifier for an Artist
 data ArtistID = ArtistID Integer deriving Show
 
+-- |Record describing a recording artist
 data Artist = Artist {
      artistId   :: ArtistID,
      artistName :: Text
@@ -81,26 +87,36 @@ instance Convertible AlbumID SqlValue where
 instance Convertible RecordingID SqlValue where
      safeConvert (RecordingID a) = safeConvert a
 
-
+-- |Convenience function for unpacking a [SqlValue]
 convert1 :: Convertible a b => [a] -> b
 convert1 (a:_) = convert a
+
+-- |Convenience function for unpacking a [SqlValue]
 convert2 :: Convertible a b => Convertible a c => [a] -> (b, c)
 convert2 (a:b:_) = (convert a, convert b)
 
+-- |Convenience function for unpacking a [SqlValue]
 convert3 :: Convertible a b => Convertible a c => Convertible a d => [a] -> (b, c, d)
 convert3 (a:b:c:_) = (convert a, convert b, convert c)
 
+-- |Convenience function for unpacking a [SqlValue]
 convert4 :: Convertible a b => Convertible a c => Convertible a d => Convertible a e => [a] -> (b, c, d, e)
 convert4 (a:b:c:d:_) = (convert a, convert b, convert c, convert d)
 
+-- |Convenience function for unpacking a [SqlValue]
 convert5 :: Convertible a b => Convertible a c => Convertible a d => Convertible a e => Convertible a f => [a] -> (b, c, d, e, f)
 convert5 (a:b:c:d:e:_) = (convert a, convert b, convert c, convert d, convert e)
 
+-- |Convenience function for unpacking a [SqlValue]
 convert6 :: Convertible a b => Convertible a c => Convertible a d => Convertible a e => Convertible a f => Convertible a g =>
             [a] -> (b, c, d, e, f, g)
 convert6 (a:b:c:d:e:f:_) = (convert a, convert b, convert c, convert d, convert e, convert f)
 
 
+-- |Opaque type refering to a database.
+-- Some safety is provided by the fact that the type constructor is not exported—for a caller to get
+-- a DatabaseRef, they must call getDatabaseRef which first checks that the database exists and creates
+-- it if necessary.
 data DatabaseRef = DatabaseRef String
 
 
@@ -108,7 +124,9 @@ printSqlError :: SqlError -> IO ()
 printSqlError e = putStrLn $ show e
 
 
-getDatabaseRef :: String -> IO (Either String DatabaseRef)
+-- |Checks that a database file exists, creates it if it doesn't, and returns a DatabaseRef to the database
+getDatabaseRef :: String                         -- ^The name of the database file
+               -> IO (Either String DatabaseRef)
 getDatabaseRef filename = do
   fileExists <- doesFileExist filename
   maybeDB <- if fileExists
@@ -121,7 +139,7 @@ getDatabaseRef filename = do
     Nothing -> return $ Left "Error"
 
 
-
+-- Given a filename, create a new Sqlite database and set up the schema
 createNewDatabase :: String -> IO Connection
 createNewDatabase filename = do
   conn <- connectSqlite3 filename
@@ -163,6 +181,9 @@ createNewDatabase filename = do
   commit conn
   return conn
 
+
+-- Opens the database, calls a function which takes a database Connection, closes the
+-- database, and returns the result of the called function.
 withDatabase :: DatabaseRef -> ( Connection -> IO b ) -> IO b
 withDatabase (DatabaseRef filename) action = do
   conn <- connectSqlite3 filename
@@ -171,9 +192,11 @@ withDatabase (DatabaseRef filename) action = do
   return r
 
 
+-- |Given the name of an artist, returns a list of all Artist records that match that name exactly.
 findArtist :: Text -> DatabaseRef -> IO [Artist]
 findArtist name db = withDatabase db $ findArtist' name
 
+-- |Like findArtist, but expects a Connection rather than a DatabaseRef.
 findArtist' :: Text -> Connection -> IO [Artist]
 findArtist' name conn = do
     r <- quickQuery' conn "SELECT id, name FROM artists WHERE name = ?;" [convert name]
@@ -181,22 +204,27 @@ findArtist' name conn = do
   where artistFromSql (idValue:nameValue:[]) = Artist (ArtistID (convert idValue)) (convert nameValue)
 
 
+-- |Given an ArtistID, retrieves the Artist record from the database.
 getArtist :: ArtistID -> DatabaseRef -> IO Artist
 getArtist a db = withDatabase db $ getArtist' a
 
+-- |Like getArtist, but expects a Connection rather than a DatabaseRef,
 getArtist' :: ArtistID -> Connection -> IO Artist
 getArtist' (ArtistID i) conn = do
     r <- quickQuery' conn "SELECT name FROM artists WHERE id = ?;" [convert i]
     case r of
       [[name]] -> return $ Artist (ArtistID i) (convert name)
 
+-- |An artist which may not yet been added to the database.
 data NewArtist = NewArtist {
      newArtistName :: Text
 }
 
+-- |Add a new Artist to the Database. If successful, returns the new Artist record.
 addArtist :: NewArtist -> DatabaseRef -> IO (Maybe Artist)
 addArtist a db = withDatabase db $ addArtist' a
 
+-- Like addArtist, but expects a Connection rather than a DatabaseRef
 addArtist' :: NewArtist -> Connection -> IO (Maybe Artist)
 addArtist' newArtist conn = do
     --newID <- fromIntegral . length <$> findArtist' (newArtistName newArtist) conn
@@ -205,6 +233,7 @@ addArtist' newArtist conn = do
     commit conn
     Just <$> getArtist' (ArtistID newID) conn
 
+-- Returns an Integer that is not currently used as an ArtistID
 getNewArtistID' :: Connection -> IO Integer
 getNewArtistID' conn = do
     r <- quickQuery' conn "SELECT artist_id FROM last_ids" []
@@ -213,19 +242,22 @@ getNewArtistID' conn = do
     run conn "UPDATE last_ids SET artist_id=?" [convert newID]
     return newID
 
-
+-- |Given the name of an Album, returns a list of all Album records that have that name.
 findAlbums :: Text -> DatabaseRef -> IO [Album]
 findAlbums title db = withDatabase db $ findAlbum' title
 
+-- Like findAlbums, but expects a Connection rather than a DatabaseRef
 findAlbum' :: Text -> Connection -> IO [Album]
 findAlbum' title conn = do
     r <- quickQuery' conn "SELECT id FROM albums WHERE title = ?;" [convert title]
     forM r $ \x ->
       getAlbum' (convert1 x) conn
 
+-- |Given an AlbumID, retrieve the corresponding Album record from the database.
 getAlbum :: AlbumID -> DatabaseRef -> IO Album
 getAlbum a db = withDatabase db $ getAlbum' a
 
+-- Like getAlbum, but expects a Connection rather than a DatabaseRef
 getAlbum' :: AlbumID -> Connection -> IO Album
 getAlbum' albumID conn = do
     r <- quickQuery' conn "SELECT title, artist, num_tracks FROM albums WHERE id = ?;" [convert albumID]
@@ -233,9 +265,11 @@ getAlbum' albumID conn = do
     artist <- getArtist' artistID conn
     return $ Album albumID title artist numTracks
 
+-- |Given a RecordingID, retrieve the corresponding Recording from the database.
 getRecording :: RecordingID -> DatabaseRef -> IO Recording
 getRecording r db = withDatabase db $ getRecording' r
 
+-- Like getRecording, but expects a Connection rather than a DatabaseRef
 getRecording' :: RecordingID -> Connection -> IO Recording
 getRecording' recordingID conn = do
     r <- quickQuery' conn "SELECT file, title, artist, album, track_number FROM recordings WHERE id = ?;" [convert recordingID]
