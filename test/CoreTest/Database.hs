6a299b97df260081977304227166f835ae4402eb
Matthew Gordon <matthew.scott.gordon@gmail.com>
2013-10-19 11:01:04 -0300

    Add experimental test fixture type. No change in functionality.
    New Fixture type is used to create and delete the temporary
    directory used by the tests.

--- test/CoreTest/Database.hs
+++ test/CoreTest/Database.hs
@@ -8,10 +8,33 @@ import Gramophone.Core.Database
 import Test.HUnit
 
 import System.Directory
-import System.IO.Temp
 import System.FilePath
+import System.IO.Error
+import Control.Exception
 
-tests = TestCase $ withSystemTempDirectory "gramophone" $ \tmpDir -> do
+
+createTempDir :: String -> IO FilePath
+createTempDir template = do
+  sysTmpDir <- getTemporaryDirectory
+  createTempDir' 0 template sysTmpDir
+    where
+      createTempDir' n base sysTmpDir = do
+                           let tmpDir = sysTmpDir </> base <.> (show n)
+                           r <- tryIOError (createDirectory tmpDir)
+                           case r of
+                             Left e -> if (isAlreadyExistsError e)
+                                       then createTempDir' (n+1) base sysTmpDir
+                                       else ioError e
+                             Right () -> return tmpDir
+
+data Fixture a b = Fixture (IO a) (a -> IO b)
+
+withFixture (Fixture setUp tearDown) = TestCase . (bracket setUp tearDown)
+
+emptyTempDirFixture template = Fixture (createTempDir template) (\dirName -> removeDirectoryRecursive dirName)
+
+
+tests = withFixture (emptyTempDirFixture "gramophone") $ \tmpDir -> do
   dbEither <- createDatabase (tmpDir </> "database")
   case dbEither of
     Left e   -> assertFailure $ show e
