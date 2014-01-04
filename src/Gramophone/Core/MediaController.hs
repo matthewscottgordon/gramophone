56d104455988f2aa5a6c5e601ddc14bb38b8b6d3
Matthew Gordon <matthew.scott.gordon@gmail.com>
2014-01-04 11:35:11 -0400

    Continuing work on MediaController. Still hashing out design.

--- src/Gramophone/Core/MediaController.hs
+++ src/Gramophone/Core/MediaController.hs
@@ -20,21 +20,98 @@
 
 module Gramophone.Core.MediaController
     (
-     {-Tags(),
-     tagTrackName,
-     tagAlbumName,
-     tagArtistName,
-     tagTrackNumber,
-     tagNumTracks,
-     tagDiscNumber,
-     tagNumDiscs,
-     emptyTags,
-
      MediaController,
      initMediaController,
-     readTagsFromFile-}
+     playFile,
+     --getCurrentState,
+
      module Gramophone.Core.MediaController.ReadTagsFromFile
     ) where
 
 import Gramophone.Core.MediaController.ReadTagsFromFile
 
+import Control.Concurrent (forkIO)
+import Control.Concurrent.MVar (MVar(..), newEmptyMVar, putMVar, takeMVar)
+--import Control.Concurrent.Chan (Chan(..), newChan, readChan)
+import Control.Monad (liftM, liftM2, (>>=), (>=>))
+import Data.Time.Clock (DiffTime(..), secondsToDiffTime, UTCTime(..),
+                        getCurrentTime)
+
+data Track = Track {
+    trackFilename    :: FilePath,
+    trackLength      :: DiffTime,
+    trackCurrentTime :: DiffTime
+  }
+
+
+data PlayerState = Playing Track | Stopped
+
+
+data Result = StatusMessage PlayerState UTCTime
+                            | PlayMessage PlayResult
+    
+
+data Command = Play FilePath | Stop | GetStatus
+
+
+data MediaController =
+  MediaController (MVar Result) (MVar Command)
+
+
+data MCState = MCState {
+    comms :: MediaController,
+    state :: PlayerState
+  }
+               
+putCommand :: MediaController -> Command -> IO ()
+putCommand (MediaController _ c) = putMVar c
+
+takeCommand :: MCState -> IO Command
+takeCommand  (MCState (MediaController _ c) _) = takeMVar c
+
+putResult :: MCState -> Result -> IO ()
+putResult  (MCState (MediaController r _) _) = putMVar r
+
+takeResult :: MediaController -> IO Result
+takeResult (MediaController r _) = takeMVar r
+
+
+updatePlayerState :: MCState -> PlayerState -> MCState
+updatePlayerState (MCState mc _) s = MCState mc s
+
+initMediaController :: IO MediaController
+initMediaController = do
+  mc <- liftM2 MediaController newEmptyMVar newEmptyMVar
+  forkIO $ mcThread $ MCState mc Stopped
+  return mc
+  
+mcThread :: MCState -> IO ()
+mcThread mcs = do
+  c <- takeCommand mcs
+  (f c >=> mcThread) mcs
+  where
+    f :: Command -> MCState -> IO MCState
+    f (Play file) = playFile' file
+    f Stop        = stopPlaying'
+    f GetStatus   = getStatus'
+
+data PlayResult = PlaySuccess
+
+playFile :: FilePath -> MediaController -> IO PlayResult
+playFile file mc = do
+  putCommand mc (Play file)
+  PlayMessage r' <- takeResult mc
+  return r'
+
+playFile' :: FilePath -> MCState -> IO MCState
+playFile' file mcs  = do
+  putStrLn ("Playing \"" ++ file ++ "\".")
+  putResult mcs (PlayMessage PlaySuccess)
+  return $ updatePlayerState mcs (Playing (Track file 0 0))
+
+stopPlaying' :: MCState -> IO MCState
+stopPlaying' = (putStrLn "Stopped." >>) . return
+
+getStatus' :: MCState -> IO MCState
+getStatus' mcs@(MCState (MediaController r _) s) =
+  liftM (StatusMessage s) getCurrentTime >>= putMVar r >> return mcs
